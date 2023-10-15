b2e7ebce45f6d288f85e3f245624c710a57f636c
owy <126146587+goldietiara@users.noreply.github.com>
2023-10-15 16:02:07 +0700

    feat: adding Router Link
    adding RouterLink on Quiz Card so when user click the Quiz they can go to the Quiz page

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,21 @@
 <script setup>
 const { quiz } = defineProps(["quiz"]);
+import { RouterLink } from "vue-router";
 </script>
 <template>
-  <div
-    class="bg-pink-100 overflow-clip grid relative rounded-lg h-[230px] w-[300px] outline-sky-500/30 outline-offset-2 outline-4 hover:outline hover:cursor-pointer"
-  >
-    <img :src="quiz.img" :alt="quiz.name" />
+  <RouterLink :to="`/Quiz/${quiz.name}`">
     <div
-      class="bg-gray-200 text-black/50 absolute bottom-0 w-full h-fit py-3 px-5 text-start place-self-end"
+      class="bg-pink-100 overflow-clip grid relative rounded-lg h-[230px] w-[300px] outline-sky-500/30 outline-offset-2 outline-4 hover:outline hover:cursor-pointer"
     >
-      <h1 class="font-semibold text-lg">
-        {{ quiz.name }}
-      </h1>
-      <p>{{ quiz.questions.length }} Questions</p>
+      <img :src="quiz.img" :alt="quiz.name" />
+      <div
+        class="bg-gray-200 text-black/50 absolute bottom-0 w-full h-fit py-3 px-5 text-start place-self-end"
+      >
+        <h1 class="font-semibold text-lg">
+          {{ quiz.name }}
+        </h1>
+        <p>{{ quiz.questions.length }} Questions</p>
+      </div>
     </div>
-  </div>
+  </RouterLink>
 </template>
