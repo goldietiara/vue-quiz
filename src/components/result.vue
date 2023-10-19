015005ff888492bd4ff8952a917b11253e2ac792
owy <126146587+goldietiara@users.noreply.github.com>
2023-10-19 17:11:24 +0700

    feat: getting parent props

--- src/components/result.vue
+++ src/components/result.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { defineProps } from "vue";
+
+const { numberOfCorrectAnswer, questions } = defineProps([
+  "numberOfCorrectAnswer",
+  "questions",
+]);
 </script>
+
 <template>
   <div class="flex flex-col justify-center items-center gap-2 text-center">
     <h1 class="text-3xl font-bold mt-20">
+      You scored {{ numberOfCorrectAnswer }} out of {{ questions }}!
     </h1>
     <p class="text-xl">Keep exploring and learning. 🚀🌟</p>
   </div>
