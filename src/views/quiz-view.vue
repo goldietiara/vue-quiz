276fa1a1653fc072ef7c1ebcb1ce0f615a6170cc
owy <126146587+goldietiara@users.noreply.github.com>
2023-10-19 17:13:58 +0700

    style: UI for quiz page

---
+++ src/views/quiz-view.vue
@@ -0,0 +1,15 @@
+<script setup>
+import QuizHeader from "../components/quiz-header.vue";
+import Question from "../components/question.vue";
+import Result from "../components/result.vue";
+</script>
+<template>
+  <main class="flex flex-col gap-5 px-40 mt-5">
+    <QuizHeader
+    />
+    <Question
+    />
+    <Result
+    />
+  </main>
+</template>
