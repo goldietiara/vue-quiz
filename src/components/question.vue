dcc9c0e5c75b116b0c34cd0f3fabf9b977ffc8be
owy <126146587+goldietiara@users.noreply.github.com>
2023-10-19 17:07:36 +0700

    style: question UI

---
+++ src/components/question.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+<template>
+  <div class="ml-5">
+    <h1 class="mt-5 font-bold text-2xl">{{ question.text }}</h1>
+    <ul
+      class="flex flex-col gap-5 mt-5"
+    >
+      <li>
+        <input
+          class="hidden peer"
+        />
+        <label
+          class="flex items-center gap-2 rounded-md px-[2px] py-[2px] outline-sky-500/50 outline-offset-2 outline-4 hover:outline hover:cursor-pointer peer-checked:outline"
+        >
+          <p
+            class="w-[40px] h-[40px] bg-gray-200 flex justify-center items-center rounded-lg"
+          >
+          </p>
+        </label>
+      </li>
+    </ul>
+  </div>
+</template>
