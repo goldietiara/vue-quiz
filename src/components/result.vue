6463d2a0bb35b40a78caee7c1ae4b7605857457a
owy <126146587+goldietiara@users.noreply.github.com>
2023-10-19 17:11:06 +0700

    style: UI for result component

---
+++ src/components/result.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+<template>
+  <div class="flex flex-col justify-center items-center gap-2 text-center">
+    <h1 class="text-3xl font-bold mt-20">
+    </h1>
+    <p class="text-xl">Keep exploring and learning. 🚀🌟</p>
+  </div>
+</template>
